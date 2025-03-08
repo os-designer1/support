f534ebe26829a5ff63dd7f49926bdf152fb6ea60
os-designer1 <Gitos5647@gmail.com>
2025-03-08 10:29:43 -0800

    Update scrept.js

--- scrept.js
+++ scrept.js
@@ -10,3 +10,14 @@ window.onload = function() {
     document.getElementById('content').style.display = 'block';
   }
   
+
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const columns = document.querySelectorAll(".footer-column h4");
+    columns.forEach(column => {
+        column.addEventListener("click", function () {
+            let parent = this.parentElement;
+            parent.classList.toggle("open");
+        });
+    });
+});
