255b653d1f7e32f96e9686a99e04f438c414158a
os-designer1 <Gitos5647@gmail.com>
2025-04-14 11:29:33 -0700

    g

--- scrept.js
+++ scrept.js
@@ -3,12 +3,7 @@ function toggleNav() {
     nav.classList.toggle("active");
 }
 
-window.onload = function() {
-    // عندما يتم تحميل كل شيء، إخفاء شاشة التحميل
-    document.getElementById('loader').style.display = 'none';
-    // إظهار المحتوى
-    document.getElementById('content').style.display = 'block';
-  }
+
   
 
 
